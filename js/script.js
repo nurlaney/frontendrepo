9b3b7653367a01094abb2ca6acb1c5a519c61b55
nurlaney <nurlaney@code.edu.az>
2019-12-08 17:58:12 +0300

    some header repairs

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
-$(".close-btn").click(function(){
-    $(".alert-pop").addClass("d-none")
+$(document).ready(function () {
+// red alert pop up
+    $(".close-btn").click(function () {
+        $(".alert-pop").addClass("d-none")
+    });
+// end red alert pop up
+
+
+    $(".dropdown").hover(function(){
+        $(this).addClass("show")
+        $(this).find(".dropdown-menu").addClass("show")
+
+    }, function(){
+        $(this).removeClass("show")
+        $(this).find(".dropdown-menu").removeClass("show")
+    } )
+
+
 });
