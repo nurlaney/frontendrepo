629a7fb59162eb323be0d31722224a6f30bedb4f
nurlaney <nurlaney@code.edu.az>
2019-12-11 14:40:21 +0300

    most popular,banner slider, why choose sections

--- js/script.js
+++ js/script.js
@@ -1,36 +1,81 @@
 $(document).ready(function () {
-    // red alert pop up
-    $(".close-btn").click(function () {
-        $(".alert-pop").addClass("d-none")
-    });
-    // end red alert pop up
+  // red alert pop up
+  $(".close-btn").click(function () {
+    $(".alert-pop").addClass("d-none")
+  });
+  // end red alert pop up
 
 
-   $(".dropdown").hover(function () {
-       $(this).addClass("show")
-       $(this).find(".dropdown-menu").addClass("show")
+  $(".dropdown").hover(function () {
+    $(this).addClass("show")
+    $(this).find(".dropdown-menu").addClass("show")
 
-   }, function () {
-       $(this).removeClass("show")
-       $(this).find(".dropdown-menu").removeClass("show")
-   })
+  }, function () {
+    $(this).removeClass("show")
+    $(this).find(".dropdown-menu").removeClass("show")
+  })
 
+  // search icon click function
 
-   $(".search-area .abc .fa-search").click(function(){
+  $(".search-area .abc .icon").click(function () {
+
+    if ($(this).hasClass("fa-search")) {
       $(".search-pop").css("visibility", "visible")
       $(".search-pop").css("opacity", "1")
+      $(this).removeClass("fa-search").addClass("fa-times")
+      $(".fa-times").css("font-size", "24px")
+      $(".fa-times").css("margin-left", "4px")
 
-      $(".search-area .abc .fa-search").removeClass("fa-search").addClass("fa-times")
-       
-   })
+    }
+    else if ($(this).hasClass("fa-times")) {
+      $(".search-pop").css("visibility", "hidden")
+      $(".search-pop").css("opacity", "0")
+      $(this).removeClass("fa-times").addClass("fa-search")
+      $(".fa-search").css("font-size", "20px")
+      $(".fa-search").css("margin-left", "0px")
+    }
+    // // search icon click function end
 
-   $(".search-area .abc .fa-times").click(function(){
-    $(".search-pop").css("visibility", "hidden")
-    $(".search-pop").css("opacity", "0")
 
-    $(".search-area .abc .fa-times").removeClass("fa-times").addClass("fa-search")
-     
- })
+   
+
+
+  })
+ 
+  // banner. 
+ $('#carousel .carousel-item img').each(function () {
+  var imgSrc = $(this).attr('src');
+  $(this).parent().css({ 'background-image': 'url(' + imgSrc + ')' });
+  $(this).remove();
+});
+
+  
+  //slick slider start
+  $('.regular').slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 6
+  });
+  // slick slider end
+
+
+  // why choose edu hover
+$("#w-ch-eduglobal .reason-box").hover(function(){
+  $(this).css("margin-top","4px")
+},function(){
+  $(this).css("margin-top","12px");
+})
+
+
+
+
+
+
+
+
+
+
+
 
 
 
